3ff1b636f794daf9428afd9ee7a3bcc290bd79bb
Charles Kawczynski <kawczynski.charles@gmail.com>
2019-09-20 10:26:10 -0700

    RTE.jl compiles

--- src/RTE.jl
+++ src/RTE.jl
@@ -1,6 +1,7 @@
 module RTE
 
 include(joinpath("rte","mo_util_array.jl"))
+include(joinpath("rte","kernels","mo_optical_props_kernels.jl"))
 include(joinpath("rte","mo_optical_props.jl"))
 
 end # module
