f26c7a96418f517865025927287891d80bfa4626
Charles Kawczynski <kawczynski.charles@gmail.com>
2019-09-20 10:49:45 -0700

    Add `mo_fluxes` (#24)
    * Translate mo_fluxes
    
    * Add mo_fluxes

--- src/RTE.jl
+++ src/RTE.jl
@@ -2,6 +2,8 @@ module RTE
 
 include(joinpath("rte","mo_util_array.jl"))
 include(joinpath("rte","kernels","mo_optical_props_kernels.jl"))
+include(joinpath("rte","kernels","mo_fluxes_broadband_kernels.jl"))
 include(joinpath("rte","mo_optical_props.jl"))
+include(joinpath("rte","mo_fluxes.jl"))
 
 end # module
