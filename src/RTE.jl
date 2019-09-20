0cd15a32f815c6f6b5bdcd6851be7dd2b65911a7
Charles Kawczynski <kawczynski.charles@gmail.com>
2019-09-20 16:14:39 -0700

    Update fortran intrinsics (#27)

--- src/RTE.jl
+++ src/RTE.jl
@@ -1,5 +1,6 @@
 module RTE
 
+include("fortran_intrinsics.jl")
 include(joinpath("rte","mo_util_array.jl"))
 include(joinpath("rte","kernels","mo_optical_props_kernels.jl"))
 include(joinpath("rte","kernels","mo_fluxes_broadband_kernels.jl"))
