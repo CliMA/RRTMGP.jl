5f683cc2f1e6aba2d7b957d419547eebde84311e
Charles Kawczynski <kawczynski.charles@gmail.com>
2019-09-20 11:16:44 -0700

    Add mo_source_functions

--- src/RTE.jl
+++ src/RTE.jl
@@ -5,5 +5,6 @@ include(joinpath("rte","kernels","mo_optical_props_kernels.jl"))
 include(joinpath("rte","kernels","mo_fluxes_broadband_kernels.jl"))
 include(joinpath("rte","mo_optical_props.jl"))
 include(joinpath("rte","mo_fluxes.jl"))
+include(joinpath("rte","mo_source_functions.jl"))
 
 end # module
