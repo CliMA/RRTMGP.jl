7052bee6df46da84ff5bd2915c4ec670c6253f74
Charles Kawczynski <kawczynski.charles@gmail.com>
2019-09-23 15:13:58 -0700

    Mo rrtmgp constants (#40)
    * Translate mo_rrtmgp_constants
    
    * Include in RRTMG

--- src/RRTMG.jl
+++ src/RRTMG.jl
@@ -4,9 +4,9 @@ module RRTMG
 
 include("fortran_intrinsics.jl")
 include(joinpath("rrtmgp","mo_util_string.jl"))
+include(joinpath("rrtmgp","mo_rrtmgp_constants.jl"))
 # include(joinpath("rrtmgp","mo_util_reorder.jl"))
 # include(joinpath("rrtmgp","mo_gas_optics.jl"))
-# include(joinpath("rrtmgp","mo_rrtmgp_constants.jl"))
 # include(joinpath("rrtmgp","kernels","mo_reorder_kernels.jl"))
 include(joinpath("rrtmgp","kernels","mo_gas_optics_kernels.jl"))
 include(joinpath("rrtmgp","mo_gas_concentrations.jl"))
