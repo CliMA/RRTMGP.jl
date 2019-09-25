c1ceba25c8ee13aec3a3a31682075112db48cd7d
skandalaCLIMA <55154978+skandalaCLIMA@users.noreply.github.com>
2019-09-25 15:56:54 -0700

    Translated o_rfmip_io.jl and made other corresponding revisions - SK (#45)

--- src/JRRTMGP.jl
+++ src/JRRTMGP.jl
@@ -1,7 +1,6 @@
 module JRRTMGP
 
 include("fortran_intrinsics.jl")
-
 include(joinpath("rte","mo_util_array.jl"))
 include(joinpath("rte","kernels","mo_optical_props_kernels.jl"))
 include(joinpath("rte","kernels","mo_fluxes_broadband_kernels.jl"))
@@ -17,5 +16,6 @@ include(joinpath("rrtmgp","mo_gas_optics.jl"))
 include(joinpath("rrtmgp","kernels","mo_gas_optics_kernels.jl"))
 include(joinpath("rrtmgp","mo_gas_concentrations.jl"))
 include(joinpath("rrtmgp","mo_gas_optics_rrtmgp.jl"))
+include(joinpath("examples","rfmip-clear-sky","mo_rfmip_io.jl"))
 
 end # module
