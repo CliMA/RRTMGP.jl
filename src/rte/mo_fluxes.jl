94c299a4030754cb52cd9c2d34b3357b5894ca2d
Charles Kawczynski <kawczynski.charles@gmail.com>
2019-11-07 08:49:17 -0800

    Refactor fluxes (#73)
    * Refactor fluxes
    
    * Clean up docs

--- src/rte/mo_fluxes.jl
+++ src/rte/mo_fluxes.jl
@@ -1,51 +1,36 @@
-# This code is part of Radiative Transfer for Energetics (RTE)
-#
-# Contacts: Robert Pincus and Eli Mlawer
-# email:  rrtmgp@aer.com
-#
-# Copyright 2015-2018,  Atmospheric and Environmental Research and
-# Regents of the University of Colorado.  All right reserved.
-#
-# Use and duplication is permitted under the terms of the
-#    BSD 3-clause license, see http://opensource.org/licenses/BSD-3-Clause
-# -------------------------------------------------------------------------------------------------
-#
-# Compute output quantities from RTE based on spectrally-resolved flux profiles
-#    This module contains an abstract class and a broadband implmentation that sums over all spectral points
-#    The abstract base class defines the routines that extenstions must implement: reduce() and are_desired()
-#    The intent is for users to extend it as required, using mo_flxues_broadband as an example
-#
-# -------------------------------------------------------------------------------------------------
+"""
+    mo_fluxes
+
+Compute output quantities from RTE based on spectrally-resolved flux profiles
+  - This module contains an abstract class and a broadband implementation that sums over all spectral points
+  - The abstract class defines the routines that extensions must implement: reduce() and are_desired()
+  - The intent is for users to extend it as required, using mo_flxues_broadband as an example
+"""
 module mo_fluxes
 
+using DocStringExtensions
 using ..fortran_intrinsics
 using ..mo_optical_props
 using ..mo_fluxes_broadband_kernels
 
 export ty_fluxes_broadband, are_desired, reduce!
 
-  # -----------------------------------------------------------------------------------------------
-  #
-  # Abstract base class
-  #   reduce() function accepts spectral flux profiles, computes desired outputs
-  #   are_desired() returns a logical - does it makes sense to invoke reduce()?
-  #
-  # -----------------------------------------------------------------------------------------------
+"""
+    ty_fluxes{FT}
 
+Abstract Fluxes struct
+"""
 abstract type ty_fluxes{FT} end
 
-  # type, abstract, public :: ty_fluxes
-  # contains
-  #   procedure(reduce_abstract),      deferred, public :: reduce
-  #   procedure(are_desired_abstract), deferred, public :: are_desired
-  # end type ty_fluxes
-  # -----------------------------------------------------------------------------------------------
-  #
-  # Class implementing broadband integration for the complete flux profile
-  #   Data components are pointers so results can be written directly into memory
-  #
-  # -----------------------------------------------------------------------------------------------
+"""
+    ty_fluxes_broadband{FT} <: ty_fluxes{FT}
 
+Contains up, down, net and direct downward fluxes
+
+# Fields
+
+$(DocStringExtensions.FIELDS)
+"""
 mutable struct ty_fluxes_broadband{FT} <: ty_fluxes{FT}
   flux_up#::Array{FT,2}
   flux_dn#::Array{FT,2}
@@ -55,127 +40,77 @@ end
 
 ty_fluxes_broadband(FT) = ty_fluxes_broadband{FT}(ntuple(i->nothing, 4)...)
 
-
-
-  # type, extends(ty_fluxes), public :: ty_fluxes_broadband
-  #   real(FT), dimension(:,:), pointer :: flux_up => NULL(), flux_dn => NULL()
-  #   real(FT), dimension(:,:), pointer :: flux_net => NULL()    # Net (down - up)
-  #   real(FT), dimension(:,:), pointer :: flux_dn_dir => NULL() # Direct flux down
-  # contains
-  #   procedure, public :: reduce      => reduce_broadband
-  #   procedure, public :: are_desired => are_desired_broadband
-  # end type ty_fluxes_broadband
-  # -----------------------------------------------------------------------------------------------
-
-  # --------------------------------------------------------------------------------------
-  #
-  # Broadband fluxes -- simply sum over the spectral dimension and report the whole profile
-  #
-  # --------------------------------------------------------------------------------------
 """
-    class(ty_fluxes_broadband),        intent(inout) :: this
-    real(kind=FT), dimension(:,:,:),   intent(in   ) :: gpt_flux_up # Fluxes by gpoint [W/m2](ncol, nlay+1, ngpt)
-    real(kind=FT), dimension(:,:,:),   intent(in   ) :: gpt_flux_dn # Fluxes by gpoint [W/m2](ncol, nlay+1, ngpt)
-    class(ty_optical_props),           intent(in   ) :: spectral_disc  #< derived type with spectral information
-    logical,                           intent(in   ) :: top_at_1
-    real(kind=FT), dimension(:,:,:), optional,
-                                       intent(in   ) :: gpt_flux_dn_dir# Direct flux down
-    character(len=128)                               :: error_msg
-    # ------
-    integer :: ncol, nlev, ngpt
-    # ------
+    reduce!(this::ty_fluxes_broadband,
+                 gpt_flux_up::A,
+                 gpt_flux_dn::A,
+                 spectral_disc::ty_optical_props,
+                 top_at_1::Bool,
+                 gpt_flux_dn_dir=nothing)
+
+Compute `ty_fluxes_broadband` `this` by summing over the
+spectral dimension, given
+
+ - `gpt_flux_up` upward fluxes by gpoint [W/m2]
+ - `gpt_flux_dn` downward fluxes by gpoint [W/m2]
+ - `spectral_disc` optical properties containing spectral information
+ - `top_at_1` bool indicating at top
+optional:
+ - `gpt_flux_dn_dir` downward direct flux
 """
-  function reduce!(this::ty_fluxes_broadband,
-                            gpt_flux_up::A,
-                            gpt_flux_dn::A,
-                            spectral_disc::ty_optical_props,
-                            top_at_1::Bool,
-                            gpt_flux_dn_dir=nothing) where A
+function reduce!(this::ty_fluxes_broadband,
+                 gpt_flux_up::Array{FT,3},
+                 gpt_flux_dn::Array{FT,3},
+                 spectral_disc::ty_optical_props,
+                 top_at_1::Bool,
+                 gpt_flux_dn_dir::Union{Nothing,Array{FT,3}}=nothing) where FT<:Real
 
-    ncol = size(gpt_flux_up, 1)
-    nlev = size(gpt_flux_up, 2)
-    ngpt = size(gpt_flux_up, 3)
+  ncol,nlev,ngpt = size(gpt_flux_up)
 
-    #
-    # Check array sizes
-    #  Input arrays
-    #
-    if any([size(gpt_flux_dn, 1) ≠ ncol, size(gpt_flux_dn, 2) ≠ nlev, size(gpt_flux_dn, 3) ≠ ngpt])
-      error("reduce: gpt_flux_dn array incorrectly sized")
-    end
-    if gpt_flux_dn_dir ≠ nothing
-      if any([size(gpt_flux_dn_dir, 1) ≠ ncol, size(gpt_flux_dn_dir, 2) ≠ nlev, size(gpt_flux_dn_dir, 3) ≠ ngpt])
-        error("reduce: gpt_flux_dn_dir array incorrectly sized")
-      end
-    end
-    #
-    # Output arrays
-    #
-    if associated(this.flux_up)
-      if any([size(this.flux_up, 1), size(this.flux_up, 2)] ≠ [ncol,nlev])
-        error("reduce: flux_up array incorrectly sized")
-      end
-    end
-    if associated(this.flux_dn)
-      if any([size(this.flux_dn, 1), size(this.flux_dn, 2)] ≠ [ncol,nlev])
-        error("reduce: flux_dn array incorrectly sized")
-      end
-    end
-    if associated(this.flux_net)
-      if any([size(this.flux_net, 1), size(this.flux_net, 2)] ≠ [ncol,nlev])
-        error("reduce: flux_net array incorrectly sized")
-      end
-    end
-    if associated(this.flux_dn_dir)
-      if any([size(this.flux_dn_dir, 1), size(this.flux_dn_dir, 2)] ≠ [ncol,nlev])
-        error("reduce: flux_dn_dir array incorrectly sized")
-      end
-    end
-    #
-    # Self-consistency -- shouldn't be asking for direct beam flux if it isn't supplied
-    if associated(this.flux_dn_dir) && !present(gpt_flux_dn_dir)
-      error("reduce: requesting direct downward flux but this hasn't been supplied")
-    end
+  # Check array sizes
+  @assert all(size(gpt_flux_dn) .== (ncol,nlev,ngpt))
+  gpt_flux_dn_dir ≠ nothing &&  @assert all(size(gpt_flux_dn_dir) .== (ncol,nlev,ngpt))
+  associated(this.flux_up) && @assert all(size(this.flux_up) .== (ncol,nlev))
+  associated(this.flux_dn) && @assert all(size(this.flux_dn) .== (ncol,nlev))
+  associated(this.flux_net) && @assert all(size(this.flux_net) .== (ncol,nlev))
+  associated(this.flux_dn_dir) && @assert all(size(this.flux_dn_dir) .== (ncol,nlev))
 
-    #
-    # Broadband fluxes - call the kernels
-    #
-    if associated(this.flux_up    )
-      this.flux_up .= sum_broadband(ncol, nlev, ngpt, gpt_flux_up)
-    end
-    if associated(this.flux_dn    )
-      this.flux_dn .= sum_broadband(ncol, nlev, ngpt, gpt_flux_dn)
-    end
-    if associated(this.flux_dn_dir)
-      this.flux_dn_dir = sum_broadband(ncol, nlev, ngpt, gpt_flux_dn_dir)
-    end
+  # Self-consistency -- shouldn't be asking for direct beam flux if it isn't supplied
+  @assert !(associated(this.flux_dn_dir) && !present(gpt_flux_dn_dir))
 
-    if associated(this.flux_net)
-      #
-      #  Reuse down and up results if possible
-      #
-      if associated(this.flux_dn) && associated(this.flux_up)
-        this.flux_net = net_broadband(ncol, nlev,      this.flux_dn, this.flux_up)
-      else
-        this.flux_net = net_broadband(ncol, nlev, ngpt, gpt_flux_dn,  gpt_flux_up)
-      end
-    end
-  end # function
-  # --------------------------------------------------------------------------------------
-  #
-  # Are any fluxes desired from this set of g-point fluxes? We can tell because memory will
-  #   be allocated for output
-  #
-  # --------------------------------------------------------------------------------------
-"""
-    class(ty_fluxes_broadband), intent(in   ) :: this
-    logical                                   :: are_desired_broadband
-"""
-  function are_desired(this::ty_fluxes_broadband)
-    return any( [associated(this.flux_up),
-                 associated(this.flux_dn),
-                 associated(this.flux_dn_dir),
-                 associated(this.flux_net)] )
+  # Broadband fluxes - call the kernels
+  if associated(this.flux_up    )
+    this.flux_up .= sum_broadband(ncol, nlev, ngpt, gpt_flux_up)
   end
-  # --------------------------------------------------------------------------------------
-end # module
+  if associated(this.flux_dn    )
+    this.flux_dn .= sum_broadband(ncol, nlev, ngpt, gpt_flux_dn)
+  end
+  if associated(this.flux_dn_dir)
+    this.flux_dn_dir = sum_broadband(ncol, nlev, ngpt, gpt_flux_dn_dir)
+  end
+
+  if associated(this.flux_net)
+
+    #  Reuse down and up results if possible
+    if associated(this.flux_dn) && associated(this.flux_up)
+      this.flux_net = net_broadband(ncol, nlev,      this.flux_dn, this.flux_up)
+    else
+      this.flux_net = net_broadband(ncol, nlev, ngpt, gpt_flux_dn,  gpt_flux_up)
+    end
+  end
+end
+
+"""
+    are_desired(this::ty_fluxes_broadband)
+
+Bool indicating if any fluxes desired from this set of
+g-point fluxes. We can tell because memory will be
+allocated for output
+"""
+are_desired(this::ty_fluxes_broadband) =
+  any( [associated(this.flux_up),
+        associated(this.flux_dn),
+        associated(this.flux_dn_dir),
+        associated(this.flux_net)] )
+
+end
