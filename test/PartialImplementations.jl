7954976863952ce9e90b593c2e18ace1f22f8366
Charles Kawczynski <kawczynski.charles@gmail.com>
2019-11-09 12:53:18 -0800

    Delete `mo_garand_atmos_io` - since it's unused (#100)
    * Move mo_garand_atmos_io to test folder
    
    * Delete mo_garand_atmos_io, since it's unused

--- test/PartialImplementations.jl
+++ test/PartialImplementations.jl
@@ -15,6 +15,5 @@ using JRRTMGP.mo_source_functions
 using JRRTMGP.mo_cloud_optics
 using JRRTMGP.mo_rte_sw
 using JRRTMGP.mo_load_cloud_coefficients
-using JRRTMGP.mo_garand_atmos_io
 
 include("mo_optical_props_kernels_nstream.jl")
