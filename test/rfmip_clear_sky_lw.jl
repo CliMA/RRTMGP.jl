b5209c696d8ba2cba86bc7a3920b0f0bee421afd
Charles Kawczynski <kawczynski.charles@gmail.com>
2019-11-11 07:48:06 -0800

    Add Benchmark suite (#103)

--- test/rfmip_clear_sky_lw.jl
+++ test/rfmip_clear_sky_lw.jl
@@ -12,48 +12,24 @@ using JRRTMGP.mo_fluxes
 using JRRTMGP.mo_load_coefficients
 using JRRTMGP.mo_rfmip_io
 using JRRTMGP.mo_source_functions
+"""
+Example program to demonstrate the calculation of shortwave radiative fluxes in clear, aerosol-free skies.
+  The example files come from the Radiative Forcing MIP (https://www.earthsystemcog.org/projects/rfmip/)
+  The large problem (1800 profiles) is divided into blocks
 
-# This code is part of RRTM for GCM Applications - Parallel (RRTMGP)
-#
-# Contacts: Robert Pincus and Eli Mlawer
-# email:  rrtmgp@aer.com
-#
-# Copyright 2015-2018,  Atmospheric and Environmental Research and
-# Regents of the University of Colorado.  All right reserved.
-#
-# Use and duplication is permitted under the terms of the
-#    BSD 3-clause license, see http://opensource.org/licenses/BSD-3-Clause
-# -------------------------------------------------------------------------------------------------
-#
-# Example program to demonstrate the calculation of shortwave radiative fluxes in clear, aerosol-free skies.
-#   The example files come from the Radiative Forcing MIP (https://www.earthsystemcog.org/projects/rfmip/)
-#   The large problem (1800 profiles) is divided into blocks
-#
-# Program is invoked as rrtmgp_rfmip_sw [block_size input_file  coefficient_file upflux_file downflux_file]
-#   All arguments are optional but need to be specified in order.
-#
-# -------------------------------------------------------------------------------------------------
-#
-# Error checking: Procedures in rte+rrtmgp return strings which are empty if no errors occured
-#   Check the incoming string, print it out and stop execution if non-empty
-#
+Program is invoked as rrtmgp_rfmip_sw [block_size input_file  coefficient_file upflux_file downflux_file]
+  All arguments are optional but need to be specified in order.
 
-function run_driver(datafolder, optical_props_constructor)
-  #
-  # Optical properties of the atmosphere as array of values
-  #   In the longwave we include only absorption optical depth (_1scl)
-  #   Shortwave calculations use optical depth, single-scattering albedo, asymmetry parameter (_2str)
-  #
-  # RTE driver uses a derived type to reduce spectral fluxes to whatever the user wants
-  #   Here we're just reporting broadband fluxes
-  #
-  # RRTMGP's gas optics class needs to be initialized with data read from a netCDF files
-  #
-  # --------------------------------------------------
-  #
-  # Local variables
-  #
+Optical properties of the atmosphere as array of values
+   In the longwave we include only absorption optical depth (_1scl)
+   Shortwave calculations use optical depth, single-scattering albedo, asymmetry parameter (_2str)
 
+RTE driver uses a derived type to reduce spectral fluxes to whatever the user wants
+  Here we're just reporting broadband fluxes
+
+RRTMGP's gas optics class needs to be initialized with data read from a netCDF files
+"""
+function rfmip_clear_sky_lw(ds, optical_props_constructor; compile_first=false)
   # character(len=132) :: flxdn_file, flxup_file
   # integer            :: nargs, ncol, nlay, nbnd, ngpt, nexp, nblocks, block_size, forcing_index
   # logical            :: top_at_1
@@ -86,26 +62,13 @@ function run_driver(datafolder, optical_props_constructor)
   deg_to_rad = acos(-FT(1))/FT(180)
   n_quad_angles = Integer.(1)
 
-  # -------------------------------------------------------------------------------------------------
   #
   # Code starts
   #   all arguments are optional
   #
 
-  clear_sky_dir = joinpath(datafolder, "examples","rfmip-clear-sky")
 
-  rfmip_file = joinpath(clear_sky_dir, "multiple_input4MIPs_radiation_RFMIP_UColorado-RFMIP-1-2_none.nc")
-  kdist_file = joinpath(datafolder, "rrtmgp", "data", "rrtmgp-data-lw-g256-2018-12-04.nc")
-
-  lw_flx_up_for_res_file = joinpath(clear_sky_dir, "rlu_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc") # Results from Fortran code
-  lw_flx_dn_for_res_file = joinpath(clear_sky_dir, "rld_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc") # Results from Fortran code
-
-  ds_lw_flx_up = Dataset(lw_flx_up_for_res_file, "r") # reading the NetCDF file in read only mode
-  ds_lw_flx_dn = Dataset(lw_flx_dn_for_res_file, "r") # reading the NetCDF file in read only mode
-  ds = Dataset(rfmip_file, "r") # reading the NetCDF file in read only mode
-  ds_k_dist = Dataset(kdist_file, "r") # reading the NetCDF file in read only mode
-
-  ncol, nlay, nexp = read_size(ds)
+  ncol, nlay, nexp = read_size(ds[:rfmip])
 
   forcing_index = 1
   block_size = 8
@@ -116,7 +79,7 @@ function run_driver(datafolder, optical_props_constructor)
   @assert mod(ncol*nexp, block_size) == 0
 
   nblocks = Int((ncol*nexp)/block_size)
-  println("Doing $(nblocks) blocks of size $(block_size)")
+  # println("Doing $(nblocks) blocks of size $(block_size)")
 
   @assert 1 <= forcing_index <= 3
 
@@ -126,9 +89,9 @@ function run_driver(datafolder, optical_props_constructor)
   #   provided by RFMIP (e.g. 'co2' and 'carbon_dioxide')
   #
 
-  kdist_gas_names, rfmip_gas_games = determine_gas_names(ds_k_dist, forcing_index)
-  print("Calculation uses RFMIP gases: ")
-  @show rfmip_gas_games
+  kdist_gas_names, rfmip_gas_games = determine_gas_names(ds[:k_dist], forcing_index)
+  # print("Calculation uses RFMIP gases: ")
+  # @show rfmip_gas_games
 
   # --------------------------------------------------
   #
@@ -137,7 +100,7 @@ function run_driver(datafolder, optical_props_constructor)
   #
   # Allocation on assignment within reading routines
   #
-  p_lay, p_lev, t_lay, t_lev = read_and_block_pt(ds, block_size)
+  p_lay, p_lev, t_lay, t_lev = read_and_block_pt(ds[:rfmip], block_size)
   #
   # Are the arrays ordered in the vertical with 1 at the top or the bottom of the domain?
   #
@@ -147,21 +110,21 @@ function run_driver(datafolder, optical_props_constructor)
   #
   # Read the gas concentrations and surface properties
   #
-  gas_conc_array = read_and_block_gases_ty(ds, block_size, kdist_gas_names, rfmip_gas_games)
-  sfc_emis, sfc_t = read_and_block_lw_bc(ds, block_size)
+  gas_conc_array = read_and_block_gases_ty(ds[:rfmip], block_size, kdist_gas_names, rfmip_gas_games)
+  sfc_emis, sfc_t = read_and_block_lw_bc(ds[:rfmip], block_size)
 
   #
   # Read k-distribution information. load_and_init() reads data from netCDF and calls
   #   k_dist%init(); users might want to use their own reading methods
   #
-  k_dist = load_and_init(ds_k_dist, gas_conc_array[1])
+  k_dist = load_and_init(ds[:k_dist], gas_conc_array[1])
 
   @assert source_is_internal(k_dist)
 
   nbnd = get_nband(k_dist.optical_props)
   ngpt = get_ngpt(k_dist.optical_props)
-  @show nbnd
-  @show ngpt
+  # @show nbnd
+  # @show ngpt
 
   #
   # RRTMGP won't run with pressure less than its minimum. The top level in the RFMIP file
@@ -199,7 +162,7 @@ function run_driver(datafolder, optical_props_constructor)
   #
   fluxes = ty_fluxes_broadband(FT)
 
-  for b = 1:nblocks
+  for b = 1:(compile_first ? 1 : nblocks)
     fup = fluxes.flux_up = @view(flux_up[:,:,b])
     fdn = fluxes.flux_dn = @view(flux_dn[:,:,b])
 
@@ -224,44 +187,32 @@ function run_driver(datafolder, optical_props_constructor)
     @assert fdn === fluxes.flux_dn
 
   end
-  # --------------------------------------------------
-  # unblock_and_write!(trim(flxup_file), "rsu", flux_up)
-  # unblock_and_write!(trim(flxdn_file), "rsd", flux_dn)
 
   # reshaping the flux_up and flux_dn arrays for comparison with Fortran code.
   flux_up = reshape_for_comparison(flux_up, nlay, ncol, nexp)
   flux_dn = reshape_for_comparison(flux_dn, nlay, ncol, nexp)
 
-  # comparing with results from fortran code
-  rlu_for = ds_lw_flx_up["rlu"][:]
-  rld_for = ds_lw_flx_dn["rld"][:]
+  # comparing with reference data
+  rlu_ref = ds[:flx_up]["rlu"][:]
+  rld_ref = ds[:flx_dn]["rld"][:]
 
-  diff_up = maximum( abs.( flux_up .- rlu_for ) )
-  diff_dn = maximum( abs.( flux_dn .- rld_for ) )
+  diff_up = maximum( abs.( flux_up .- rlu_ref ) )
+  diff_dn = maximum( abs.( flux_dn .- rld_ref ) )
 
-  diff_up_ulps = maximum( abs.( flux_up .- rlu_for ) ./ eps.(rlu_for) )
-  diff_dn_ulps = maximum( abs.( flux_dn .- rld_for ) ./ eps.(rld_for) )
+  diff_up_ulps = maximum( abs.( flux_up .- rlu_ref ) ./ eps.(rlu_ref) )
+  diff_dn_ulps = maximum( abs.( flux_dn .- rld_ref ) ./ eps.(rld_ref) )
 
   # @show sqrt(1/eps(FT))
-  # @show diff_up, diff_up_ulps, maximum(abs.(rlu_for))
-  # @show diff_dn, diff_dn_ulps, maximum(abs.(rld_for))
+  # @show diff_up, diff_up_ulps, maximum(abs.(rlu_ref))
+  # @show diff_dn, diff_dn_ulps, maximum(abs.(rld_ref))
 
-  if optical_props_constructor isa ty_optical_props_2str
-    @test diff_up_ulps < sqrt(1/(1e6eps(FT)))
-    @test diff_dn_ulps < sqrt(1/(1e6eps(FT)))
-  else
-    @test diff_up_ulps < sqrt(1/(1e5eps(FT)))
-    @test diff_dn_ulps < sqrt(1/(1e3eps(FT)))
+  if !compile_first
+    if optical_props_constructor isa ty_optical_props_2str
+      @test diff_up_ulps < sqrt(1/(1e6eps(FT)))
+      @test diff_dn_ulps < sqrt(1/(1e6eps(FT)))
+    else
+      @test diff_up_ulps < sqrt(1/(1e5eps(FT)))
+      @test diff_dn_ulps < sqrt(1/(1e3eps(FT)))
+    end
   end
-
-  close(ds_lw_flx_up)
-  close(ds_lw_flx_dn)
-  close(ds)
-  close(ds_k_dist)
-end
-
-@testset "Longwave driver" begin
-  datafolder = JRRTMGP.data_folder_rrtmgp()
-  run_driver(datafolder, ty_optical_props_2str)
-  run_driver(datafolder, ty_optical_props_1scl)
 end
