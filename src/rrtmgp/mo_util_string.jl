6eba496c0775ff7e83778feff804fb532209e527
Charles Kawczynski <kawczynski.charles@gmail.com>
2019-11-15 08:15:44 -0800

    Simplify string_loc_in_array

--- src/rrtmgp/mo_util_string.jl
+++ src/rrtmgp/mo_util_string.jl
@@ -10,16 +10,6 @@ export string_loc_in_array
 Location of string `s` in the array `array`. If the
 string is not in the array, returns -1.
 """
-function string_loc_in_array(s, array)
-  s_loc_in_array = -1
-  lc_string = lowercase(trim(s))
-  for i in eachindex(array)
-    if lc_string == lowercase(trim(array[i]))
-      s_loc_in_array = i
-      break
-    end
-  end
-  return s_loc_in_array
-end
+string_loc_in_array(s, array) = s in array ? findfirst(i->i==s,array) : -1
 
 end
