6bc742348d5584fbea592619144ff25014cb1439
skandalaCLIMA <skandala@caltech.edu>
2021-01-04 14:35:04 -0800

    Replaced old rrtmgp and src directories. Removed MeshOrientations.jl, Misc.jl and Utilities.jl, which are no longer used. Removed old tests.

--- src/RRTMGP.jl
+++ src/RRTMGP.jl
@@ -2,35 +2,15 @@ module RRTMGP
 
 include("Device.jl")
 
-include("Misc.jl")
-include("Utilities.jl")
-include("MeshOrientations.jl")
+include(joinpath("optics", "GrayAngularDiscretizations.jl"))
+include(joinpath("optics", "GrayOptics.jl"))
+include(joinpath("optics", "GraySources.jl"))
+include(joinpath("optics", "GrayFluxes.jl"))
+include(joinpath("optics", "GrayAtmosphericStates.jl"))
+include(joinpath("optics", "GrayUtils.jl"))
+include(joinpath("optics", "GrayBCs.jl"))
+include(joinpath("optics", "GrayAtmos.jl"))
 
-include(joinpath("rte", "AngularDiscretizations.jl"))
-include(joinpath("rte", "SolarZenithAngle.jl"))
-include(joinpath("rte", "OpticalProps.jl"))
-include(joinpath("rte", "Fluxes.jl"))
-include(joinpath("rte", "SourceFunctions.jl"))
-include(joinpath("rte", "RadiativeBoundaryConditions.jl"))
-include(joinpath("extensions", "CloudOptics.jl"))
-
-include(joinpath("rte", "RTESolver.jl"))
-
-include(joinpath("rrtmgp", "Gases.jl"))
-include(joinpath("rrtmgp", "ReferenceStates.jl"))
-include(joinpath("rrtmgp", "GasConcentrations.jl"))
-include(joinpath("rrtmgp", "AtmosphericStates.jl"))
-include(joinpath("rrtmgp", "GasOptics.jl"))
-
-include(joinpath("gray_optics", "GrayAngularDiscretizations.jl"))
-include(joinpath("gray_optics", "GrayOptics.jl"))
-include(joinpath("gray_optics", "GraySources.jl"))
-include(joinpath("gray_optics", "GrayFluxes.jl"))
-include(joinpath("gray_optics", "GrayAtmosphericStates.jl"))
-include(joinpath("gray_optics", "GrayUtils.jl"))
-include(joinpath("gray_optics", "GrayBCs.jl"))
-include(joinpath("gray_optics", "GrayAtmos.jl"))
-
-include(joinpath("gray_rte", "GrayRTESolver.jl"))
+include(joinpath("rte", "GrayRTESolver.jl"))
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,4 @@
 using Test
 ENV["DATADEPS_ALWAYS_ACCEPT"] = true
 
-# Only run the per-grid-point tests
-pgp_only = false
-
-# include(joinpath("OriginalRRTMGP","AutoRunRRTMGP.jl"))
-
-for submodule in [
-    "test_allsky",
-    "test_rfmip_clear_sky_lw",
-    "test_rfmip_clear_sky_sw",
-    "solar_zenith_angle",
-    # "Benchmarks",
-    joinpath("..", "PartialImplementations", "PartialImplementations"), # compile only
-]
-    println("Testing $submodule")
-    include(joinpath(submodule * ".jl"))
-end
-
 include("gray_atm.jl")
