95ccf227a416ac081849e7c288ef5ba9b0b4105a
sriharshakandala <sriharsha.kvs@gmail.com>
2023-07-10 16:13:12 -0700

    Update all_sky tests to be able to use user-prescribed number of columns

--- test/all_sky.jl
+++ test/all_sky.jl
@@ -2,8 +2,8 @@ include("all_sky_utils.jl")
 
 context = ClimaComms.context()
 @testset "Cloudy (all-sky, Two-stream calculations using lookup table method" begin
-    @time all_sky(context, TwoStream, Float64; use_lut = true, cldfrac = Float64(1))
+    @time all_sky(context, TwoStream, Float64; ncol = 128, use_lut = true, cldfrac = Float64(1))
 end
 @testset "Cloudy (all-sky), Two-stream calculations using Pade method" begin
-    @time all_sky(context, TwoStream, Float64; use_lut = false, cldfrac = Float64(1))
+    @time all_sky(context, TwoStream, Float64; ncol = 128, use_lut = false, cldfrac = Float64(1))
 end
