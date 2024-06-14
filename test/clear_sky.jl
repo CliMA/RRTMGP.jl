b29db974a14414ef2f76b1ec37f6b6da0d6ccd40
sriharshakandala <sriharsha.kvs@gmail.com>
2024-06-14 09:53:05 -0700

    Add non-scattering longwave test to `clear-sky` solver

--- test/clear_sky.jl
+++ test/clear_sky.jl
@@ -3,6 +3,11 @@ FT = get(ARGS, 1, Float64) == "Float32" ? Float32 : Float64
 include("clear_sky_utils.jl")
 
 context = ClimaComms.context()
-@testset "testing clear sky 2-stream solver" begin
+
+@testset "clear-sky: NoScatLWRTE (OneScalar optics) & TwoStreamSWRTE (TwoStream optics)" begin
+    @time clear_sky(context, OneScalar, TwoStream, NoScatLWRTE, TwoStreamSWRTE, VmrGM, FT)
+end
+
+@testset "clear-sky: TwoStreamLWRTE (TwoStream optics) & TwoStreamSWRTE (TwoStream optics)" begin
     @time clear_sky(context, TwoStream, TwoStream, TwoStreamLWRTE, TwoStreamSWRTE, VmrGM, FT)
 end
